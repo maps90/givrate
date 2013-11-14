b8e3b0383e39777259ec2c4ec11fc151261927c4
Arie Sutanto <asutanto@xintesa.com>
2013-11-14 18:14:19 +0700

    set avg to left

--- View/Helper/GivrateHelper.php
+++ View/Helper/GivrateHelper.php
@@ -77,7 +77,7 @@ class GivrateHelper extends AppHelper {
 		unset($options['votelink']);
 
 		$point = empty($result['RateCalculate'][$field]) ? 0 : $point;
-		$point = $this->Html->div('avg', $point) . $link;
+		$point = $this->Html->div('avg left', $point) . $link;
 		return $point;
 	}
 
